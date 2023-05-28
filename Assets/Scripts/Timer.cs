802523dca50184fb2f4e80688c45324bc3561030
unknown <fadl2009gh@gmail.com>
2023-05-28 16:33:30 +0300

    Added Timer and edited gitignorte

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI timerText;
+    private float timer;
+
+    private void Start()
+    {
+        timerText = GetComponent<TextMeshProUGUI>();
+        timer = 0f;
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+
+        int minutes = Mathf.FloorToInt(timer / 60f);
+        int seconds = Mathf.FloorToInt(timer % 60f);
+
+        string timerString = string.Format("{0:00}:{1:00}", minutes, seconds);
+
+        timerText.text = timerString;
+    }
+}
+
